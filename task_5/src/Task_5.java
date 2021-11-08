48988048c5f6a2b737f0e2a7feae96b9cd6b2358
127_panfilova_valeria <61296657+varenik37@users.noreply.github.com>
2021-11-08 03:24:54 +0300

    Create Task_5.java

---
+++ task_5/src/Task_5.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Task_5 implements Task_5_base {
+    @Override
+    public ArrayList<Integer> subtask_1_ArrayList(ArrayList<Integer> data, int k, int n) {
+        // Выбрать из данного списка элементы, кратные k, но не кратные n
+        // и сформировать из них новый список.
+        // Вернуть новый список в качестве результата
+        return null;
+    }
+
+    @Override
+    public ArrayList<Integer> subtask_2_ArrayList(int size) {
+        // сгенерировать и вернуть список размера size,
+        // содержащий первые size элементов последовательности, описанной в
+        // задаче subtask_2_for задания task_3
+        return null;
+    }
+
+    @Override
+    public HashSet<Integer> subtask_3_HashSet(HashSet<Integer> s1, HashSet<Integer> s2) {
+        // найдите пересечение множеств s1 и s2
+        return null;
+    }
+
+    @Override
+    public HashSet<Integer> subtask_4_HashSet(HashSet<Integer> s1, HashSet<Integer> s2) {
+        // найдите объединение множеств s1 и s2
+        return null;
+    }
+
+    @Override
+    public HashSet<Integer> subtask_5_HashSet(HashSet<Integer> s1, HashSet<Integer> s2) {
+        // найдите дополнение множества s1 до множества s2
+        return null;
+    }
+
+    @Override
+    public HashSet<Integer> subtask_6_HashSet(HashSet<Integer> s1, HashSet<Integer> s2) {
+        // постройте множество, содержащее элементы, содержащиеся либо только в s1,
+        // либо только в s2, но не в обоих множествах одновременно
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Double> subtask_7_HashMap(ArrayList<String> data) {
+        // Дан список строк. Построить словарь, содержащий частоты слов
+        // для данного списка в процентах
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Double> subtask_8_HashMap(ArrayList<Double> data) {
+        // Дан список чисел. Сформировать словарь, содержащий среднее,
+        // максимальное и минимальное значения из данного списка. Ключи
+        // словаря "mean", "max", "min" соответственно
+        return null;
+    }
+}
