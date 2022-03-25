11c4e644c4e8f2cd3efba47537902987ce715fda
127_panfilova_valeria <61296657+varenik37@users.noreply.github.com>
2022-03-25 16:19:04 +0300

    hw

--- 2sem/hw/task_1/127_Panfilova_task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/127_Panfilova_task_1/src/Task1_Tests.java
@@ -1,5 +1,32 @@
 import org.junit.*;
 
-public class Task1_Tests {
+public class Task1_Tests extends Assert {
+    @Test
+    public void createList_createListWithNothing_ListIsEmpty() {
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        assertTrue(list.isEmpty());
+    }
 
+    @Test
+    public void pushFront_PushFrontToEmptyList_DataIsPushed() {
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushFront(0);
+        Node<Integer> Head = (Node<Integer>) list.getHead();
+        Node<Integer> Tail = (Node<Integer>) list.getTail();
+        assertEquals(0, Head.getData().intValue());
+        assertEquals(0, Tail.getData().intValue());
+    }
+
+    private int getIntDataValue(ListNode<Integer> item) {
+        return ((Node<Integer>) item).getData();
+    }
+
+    @Test
+    public void pushFront_PushFrontToNotEmptyList_DataIsPushed() {
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushFront(0);
+        list.pushFront(1);
+        list.pushFront(2);
+        assertEquals(1, getIntDataValue(list.get(1)));
+    }
 }
